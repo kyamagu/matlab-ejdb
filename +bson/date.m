1ce4ac9a5cfb94cf237ec2a83c6e77df848478a4
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2013-09-21 00:35:06 -0400

    Updated documentation

--- +bson/date.m
+++ +bson/date.m
@@ -9,15 +9,17 @@ classdef date
     function this = date(varargin)
       %DATE Create a new date object.
       %
-      %    bson.date
-      %    bson.date('2007-01-01')
+      %    date_value = bson.date
+      %    date_value = bson.date('2007-01-01')
       %
       % See also datenum
-      if nargin == 1 && isstruct(varargin{1})
-        this = repmat(this, size(varargin{1}));
-        [this.number] = deal(varargin{1}.number);
-      elseif nargin > 0
-        this.number = datenum(varargin{:});
+      if nargin > 0
+        if isstruct(varargin{1})
+          this = repmat(this, size(varargin{1}));
+          [this.number] = deal(varargin{1}.number);
+        else
+          this.number = datenum(varargin{:});
+        end
       else
         this.number = now;
       end
@@ -26,10 +28,19 @@ classdef date
     function value = string(this, varargin)
       %STRING Convert to a string representation.
       %
-      %    value.string('yyyy-mm-dd')
+      %    date_value.string('yyyy-mm-dd')
       %
       % See also datestr
       value = datestr([this.number], varargin{:});
+    end
+    
+    function value = vector(this, varargin)
+      %STRING Convert to a vector representation.
+      %
+      %    date_value.vector
+      %
+      % See also datevec
+      value = datevec([this.number], varargin{:});
     end
     
     function disp(this)
@@ -39,12 +50,27 @@ classdef date
     
     function value = double(this)
       %DOUBLE Convert to double.
+      %
+      %    double(date_value)
+      %
+      % See also datenum
       value = double(reshape([this.number], size(this)));
     end
     
     function value = char(this)
-      %CHAR Convert to string.
+      %CHAR Convert to char.
+      %
+      %    char(date_value)
+      %
       value = datestr([this.number]);
+    end
+    
+    function value = struct(this)
+      %CHAR Convert to char.
+      %
+      %    char(date_value)
+      %
+      value = reshape(struct('number', {this.number}), size(this));
     end
   end
 
